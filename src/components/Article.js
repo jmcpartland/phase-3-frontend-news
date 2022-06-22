6e751a3de7da1604273e52c6b061922e3bccf726
Joseph McPartland <joemcpartland@gmail.com>
2022-06-22 17:21:18 -0600

    added form elements to ArticleForm

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 function Article({article}) {
+
+    const [currentArticle, setCurrentArticle] = useState(0) // NOT WORKING
 
     let navigate = useNavigate();
 
@@ -10,15 +12,36 @@ function Article({article}) {
         navigate("/articles/new", article={article});
     }
 
+
+
     const deleteArticle = (e) => {
+        setCurrentArticle(article.id)  // NOT WORKING
+        console.log(currentArticle)
+
         fetch(`http://localhost:9292/articles/${article.id}`, {
             method: "DELETE",
             headers: {
                 'Content-type': 'application/json'
             }
         })
+        .then(() => setCurrentArticle(0))  // NOT WORKING
+        console.log(currentArticle)
     }
 
+
+
+
+
+    const updateArticle = (e) => {
+
+        fetch(`http://localhost:9292/articles/${article.id}`, {
+            method: "PUT",
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify()
+        })
+    }
 
     return (
         <div className="center">
@@ -28,6 +51,7 @@ function Article({article}) {
             <span className="labelStyle">Author: </span> {article.author} <p />
             <span className="labelStyle">Description: </span> {article.description}  <p />
             <button type="button" onClick={newArticle}>New</button> 
+            <button type="button" onClick={updateArticle}>Update</button>
             <button type="button" onClick={deleteArticle}>Delete</button>
         </div>
     )
--- src/components/ArticleForm.js
+++ src/components/ArticleForm.js
@@ -3,13 +3,33 @@ import { useParams } from 'react-router-dom';
 
 function ArticleForm({article}) {
     const [newArticle, setNewArticle] = useState([]);
+    const [articleTitle, setArticleTitle] = useState("")
 
     let params = useParams();
-    console.log(article);
+    // console.log(article);
+
+
+    function handleChange(event) {
+        setArticleTitle(event.target.value)
+        console.log(event.target.value)
+    }
+
+    function handleSubmit(event) {
+        // console.log(event)
+    }
+
 
     return (
         <div className="center">
             <h1>ArticleForm</h1>
+
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Article Title: 
+                    <input value={articleTitle} onChange={handleChange} />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
         </div>
     )
 }
